24bd0147014e87bdfed7d3bb1a30951907559b1a
djokodev <djokodev75@gmail.com>
2024-06-09 14:57:15 +0100

    [feat]: model issue and comment created and migrations applied

--- issue/models.py
+++ issue/models.py
@@ -11,8 +11,8 @@ class Issue(models.Model):
     priority = models.CharField(max_length=50, choices=[('LOW', 'Low'), ('MEDIUM', 'Medium'), ('HIGH', 'High')])
     nature = models.CharField(max_length=50, choices=[('BUG', 'Bug'), ('FEATURE', 'Feature'), ('TASK', 'Task')])
     status = models.CharField(max_length=50, choices=[('TODO', 'To Do'), ('IN_PROGRESS', 'In Progress'), ('FINISHED', 'Finished')])
-    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='assigned_issue')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_issue')
     created_time = models.DateTimeField(auto_now_add=True)
 
 
--- issue/urls.py
+++ issue/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from .views import IssueCreate
 
 urlpatterns = [
-    path("create", IssueCreate.as_view(), name="create-issue"),
 ]
