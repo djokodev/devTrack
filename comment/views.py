6c4a826190bb80bc8a895e0d84082cfad971b2c6
M@teo22 <djokodev75@gmail.com>
2024-07-02 16:29:08 +0200

    [feat]: build and push docker image

--- comment/views.py
+++ comment/views.py
@@ -8,16 +8,6 @@ from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
 
 
-# class CacheListRetrieveMixin(ListModelMixin, RetrieveModelMixin):
-#     @method_decorator(cache_page(60 * 20))
-#     def list(self, request, *args, **kwargs):
-#         return super().list(request, *args, **kwargs)
-    
-#     @method_decorator(cache_page(60 * 20))
-#     def retrieve(self, request, *args, **kwargs):
-#         return super().retrieve(request, *args, **kwargs)
-    
-
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
