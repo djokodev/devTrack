cdec21622b26d2cf9cc89e10acb7fa05d48233a8
M@teo22 <djokodev75@gmail.com>
2024-07-13 10:21:08 +0200

    feat: add a test in project

--- project/tests.py
+++ project/tests.py
@@ -8,23 +8,23 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
 
-# class ProjectViewSetTests(APITestCase):
-#     @classmethod
-#     def setUpTestData(cls):
-#         cls.user = User.objects.create_user(username='testuser', password='testpass')
-#         cls.token = RefreshToken.for_user(cls.user)
+class ProjectViewSetTests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(username='testuser', password='testpass')
+        cls.token = RefreshToken.for_user(cls.user)
 
-#     def setUp(self):
-#         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token.access_token}')
-#         self.url = reverse('project-list')
+    def setUp(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token.access_token}')
+        self.url = reverse('project-list')
 
-#     def test_create_project(self):
-#         data = {
-#             'name': 'Test Project',
-#             'description': 'A test project',
-#             'type': 'back-end',
-#         }
-#         response = self.client.post(self.url, data, format='json')
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-#         self.assertEqual(Project.objects.count(), 1)
-#         self.assertEqual(Project.objects.get().name, 'Test Project')
+    def test_create_project(self):
+        data = {
+            'name': 'Test Project',
+            'description': 'A test project',
+            'type': 'back-end',
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Project.objects.count(), 1)
+        self.assertEqual(Project.objects.get().name, 'Test Project')
